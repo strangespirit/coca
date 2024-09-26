150203464b4907cc10c9e26474791c099065698a
strangespirit <strangespirittt@gmail.com>
2024-09-26 23:14:28 +0300

    created section faq on hero page, section hero on contact page

---
+++ src/js/contact.js
@@ -0,0 +1,8 @@
+import '../assets/scss/contact.scss';
+
+import { useTheme } from './components/theme.js';
+useTheme()
+import { useBurger } from './components/burger.js';
+useBurger();
+import { usePartnersSlider } from './components/home/slider.js';
+usePartnersSlider();
