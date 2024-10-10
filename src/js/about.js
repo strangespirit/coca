f2d81123d572c7da655f543e1ffcb0450f4b0129
strangespirit <strangespirittt@gmail.com>
2024-10-10 18:07:46 +0300

    created page about and section hero, fixed fonts

---
+++ src/js/about.js
@@ -0,0 +1,8 @@
+import '/scss/about.scss';
+
+import { useTheme } from './components/theme.js';
+useTheme()
+import { useBurger } from './components/burger.js';
+useBurger();
+import { useAccordeon } from './components/accordeon.js';
+useAccordeon();
